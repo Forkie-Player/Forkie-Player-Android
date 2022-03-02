e6cc1104043a557586c141f4e68314d2481d5111
minha721 <2minha721@gmail.com>
2022-03-02 17:37:22 +0900

    [FEAT] Apply playlist add button

--- app/src/main/java/com/example/forkieplayer/MainActivity.kt
+++ app/src/main/java/com/example/forkieplayer/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.forkieplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.forkieplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBottom.setOnClickListener {
+            Toast.makeText(this, "Bottom Button Click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
