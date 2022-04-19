8a95670cb836449556f0f67d25705b32b8f8eb01
minha721 <2minha721@gmail.com>
2022-04-19 20:46:55 +0900

    [FEAT] Apply dark mode restrictions

--- app/src/main/java/com/example/forkieplayer/sign/IntroActivity.kt
+++ app/src/main/java/com/example/forkieplayer/sign/IntroActivity.kt
@@ -3,6 +3,7 @@ package com.example.forkieplayer.sign
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.forkieplayer.databinding.ActivityIntroBinding
 import com.example.forkieplayer.profile.ProfileActivity
 
@@ -14,6 +15,9 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 다크모드 제한
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         binding.btnSignin.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
